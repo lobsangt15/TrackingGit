9d7b8ccc08ad4ac467db788801fb2baeeb262977
BT_1N3_15 <sample@bths.edu>
2024-10-25 10:08:17 -0400

    added some logic

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,14 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Its friday!");
+        System.out.println("Hello World!");
+        double x = 0;
+        x += (Math.random() * 10)+1;
+        if ( x > 0) {
+            x++;
+        } else {
+            x--;
+        }
+        System.out.println(x);
     }
 }
