c1e460b14c9a4cbffb40a5cd79f47f548b10ea6b
BT_1N3_15 <sample@bths.edu>
2024-10-25 10:28:57 -0400

    print statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,13 +2,16 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        double x = 0;
-        x += (Math.random() * 10)+1;
-        if ( x > 0) {
-            x++;
+        double y = 0;
+        y += (Math.random() * 10)+1;
+        if ( y > 0) {
+            y++;
+            System.out.println(y);
         } else {
-            x--;
+            y--;
+            System.out.println(y);
         }
-        System.out.println(x);
+        System.out.println("Goodbye!");
+        System.out.println("See you on monday!");
     }
 }
